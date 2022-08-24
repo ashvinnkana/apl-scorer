bf1b5e304b4e531035675f1cad1c29dd5b6276d3
akan8795 <akan8795@sysco.com>
2022-08-25 00:34:41 +0530

    remove demo apps and update nav bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-import { TutorialsListComponent } from './components/tutorials-list/tutorials-list.component';
-import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
 import { ScorerDashboardComponent } from './components/scorer-dashboard/scorer-dashboard.component';
 import { TeamManagementComponent } from './components/team-management/team-management.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'scorer-dashboard', pathMatch: 'full' },
-  { path: 'tutorials', component: TutorialsListComponent },
-  { path: 'add', component: AddTutorialComponent },
-  { path: 'scorer-dashboard', component: ScorerDashboardComponent },
-  { path: 'teams', component: TeamManagementComponent },
+  { path: '', redirectTo: 'live-match', pathMatch: 'full' },
+  { path: 'live-match', component: ScorerDashboardComponent },
+  { path: 'players', component: TeamManagementComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,18 +8,12 @@ import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
-import { TutorialDetailsComponent } from './components/tutorial-details/tutorial-details.component';
-import { TutorialsListComponent } from './components/tutorials-list/tutorials-list.component';
 import { ScorerDashboardComponent } from './components/scorer-dashboard/scorer-dashboard.component';
 import { TeamManagementComponent } from './components/team-management/team-management.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddTutorialComponent,
-    TutorialDetailsComponent,
-    TutorialsListComponent,
     ScorerDashboardComponent,
     TeamManagementComponent
   ],
